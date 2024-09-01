991c6695bb32be080c1c7f2685e4f9b2b63c27cd
Dawid Cute <dawid.marcinkowski@strategiczni.pl>
2024-09-01 11:26:12 +0200

    Update CORS policy

--- src/app.js
+++ src/app.js
@@ -9,13 +9,32 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// app.use(
+//   cors({
+//     origin: 'https://strategiczny-kr-senuto.onrender.com/',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   })
+// );
+
+const allowedOrigins = ['https://strategiczny-kr-senuto.onrender.com'];
+
 app.use(
   cors({
-    origin: 'https://strategiczny-kr-senuto.onrender.com/',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    origin: function (origin, callback) {
+      // Pozwól żądaniom bez origin (np. mobilne aplikacje, narzędzia curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // Pozwól na przekazywanie credentials (np. cookies)
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
