e55bf63d12396f349ff56acdd82101794c7c8b67
Dawid Cute <dawid.marcinkowski@strategiczni.pl>
2024-09-01 11:08:29 +0200

    Update CORS

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: '*', // lub adres Twojego frontendu
+    origin: 'https://strategiczny-kr-senuto.onrender.com/',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   })
